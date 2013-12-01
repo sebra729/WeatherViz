0154b3ae7d5950c1861b21fb9e970dd94994af64
Valross <hamax109@student.liu.se>
2013-12-01 19:29:58 +0100

    Penis.erect()

--- helloworld.py
+++ helloworld.py
@@ -3,34 +3,42 @@ import maya.OpenMaya as OpenMaya
 import maya.OpenMayaMPx as OpenMayaMPx
 import maya.cmds as Philip
 
-kPluginCmdName = "spHelloWorld"
+kPluginCmdName = "erect"
 
 # Command
 class scriptedCommand(OpenMayaMPx.MPxCommand):
-    def __init__(self):
-        OpenMayaMPx.MPxCommand.__init__(self)
+	def __init__(self):
+		OpenMayaMPx.MPxCommand.__init__(self)
         
     # Invoked when the command is run.
-    def doIt(self,argList):
-        Philip.sphere()
+	def doIt(self,argList):
+		Philip.polySphere()
+		Philip.move(2,0,0)
+		Philip.polySphere()
+		Philip.move(-2,0,0)
+		Philip.polyCylinder(h=5)
+		Philip.move(0,2.5,0)
+		Philip.polySphere()
+		Philip.move(0,5,0)
+
 
 # Creator
 def cmdCreator():
-    return OpenMayaMPx.asMPxPtr( scriptedCommand() )
+	return OpenMayaMPx.asMPxPtr( scriptedCommand() )
     
 # Initialize the script plug-in
 def initializePlugin(mobject):
-    mplugin = OpenMayaMPx.MFnPlugin(mobject)
-    try:
-        mplugin.registerCommand( kPluginCmdName, cmdCreator )
-    except:
-        sys.stderr.write( "Failed to register command: %s\n" % kPluginCmdName )
-        raise
+	mplugin = OpenMayaMPx.MFnPlugin(mobject)
+	try:
+		mplugin.registerCommand( kPluginCmdName, cmdCreator )
+	except:
+		sys.stderr.write( "Failed to register command: %s\n" % kPluginCmdName )
+		raise
 
 # Uninitialize the script plug-in
 def uninitializePlugin(mobject):
-    mplugin = OpenMayaMPx.MFnPlugin(mobject)
-    try:
-        mplugin.deregisterCommand( kPluginCmdName )
-    except:
-        sys.stderr.write( "Failed to unregister command: %s\n" % kPluginCmdName )
+	mplugin = OpenMayaMPx.MFnPlugin(mobject)
+	try:
+		mplugin.deregisterCommand( kPluginCmdName )
+	except:
+		sys.stderr.write( "Failed to unregister command: %s\n" % kPluginCmdName )
